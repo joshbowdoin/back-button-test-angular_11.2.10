f3c8399af651ba4b3032cec8a23b2f389a6428f0
joshbowdoin <joshbowdoin@users.noreply.github.com>
2021-04-23 09:54:06 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'other-detail',
+    loadChildren: () => import('./other-detail/other-detail.module').then(m => m.OtherDetailModule)
+  },
+
 ];
 @NgModule({
   imports: [
@@ -13,4 +18,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
